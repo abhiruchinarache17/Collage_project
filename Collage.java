eef3820afcc9ab612618776ff6ea8b3403e79034
abhiruchinarache17 <106548838+abhiruchinarache17@users.noreply.github.com>
2024-07-23 18:08:11 +0530

    Update Collage.java

--- Collage.java
+++ Collage.java
@@ -1,4 +1,4 @@
-package equ.cjc.springcollageapplication.Model;
+package equ.cjc.StudentManagement.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -6,32 +6,77 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
-public class Collage 
+public class Student 
 {
 	@Id
-	@GeneratedValue(strategy= GenerationType.IDENTITY)
-	private String Student;
-	private String FacultyMember;
-	private String Administrator;
-	public String getStudent() {
-		return Student;
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int studentId;
+	private String studentFullName;
+	private String studentEmail;
+	private int studentAge;
+	private String studentCollageName;
+	private String studentCourse;
+	private String batchNumber;
+	private double fessPaid;
+	public double getFessPaid() {
+		return fessPaid;
 	}
-	public void setStudent(String student) {
-		Student = student;
+	public void setFessPaid(double fessPaid) {
+		this.fessPaid = fessPaid;
 	}
-	public String getFacultyMember() {
-		return FacultyMember;
+	private String batchMode;
+	public String getBatchMode() {
+		return batchMode;
 	}
-	public void setFacultyMember(String facultyMember) {
-		FacultyMember = facultyMember;
+	public void setBatchMode(String batchMode) {
+		this.batchMode = batchMode;
 	}
-	public String getAdministrator() {
-		return Administrator;
+	public int getstudentId() {
+		return studentId;
 	}
-	public void setAdministrator(String administrator) {
-		Administrator = administrator;
+	public void setstudentId(int studentId) {
+		studentId = studentId;
+	}
+	public String getStudentFullName() {
+		return studentFullName;
+	}
+	public void setStudentFullName(String studentFullName) {
+		this.studentFullName = studentFullName;
+	}
+	public String getStudentEmail() {
+		return studentEmail;
+	}
+	public void setStudentEmail(String studentEmail) {
+		this.studentEmail = studentEmail;
+	}
+	public int getStudentAge() {
+		return studentAge;
+	}
+	public void setStudentAge(int studentAge) {
+		this.studentAge = studentAge;
+	}
+	public String getStudentCollageName() {
+		return studentCollageName;
+	}
+	public void setStudentCollageName(String studentCollageName) {
+		this.studentCollageName = studentCollageName;
+	}
+	public String getStudentCourse() {
+		return studentCourse;
+	}
+	public void setStudentCourse(String studentCourse) {
+		this.studentCourse = studentCourse;
+	}
+	public String getBatchNumber() {
+		return batchNumber;
+	}
+	public void setBatchNumber(String batchNumber) {
+		this.batchNumber = batchNumber;
 	}
 	
+	
+	
+		
 	
 	
 	
